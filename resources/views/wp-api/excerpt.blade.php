9d535864890c36f8b867dadad554b1b55fce011b
white-cat-dev <alexandra.19.98.study@gmail.com>
2020-05-27 16:38:51 +0800

    updated

--- resources/views/wp-api/excerpt.blade.php
+++ resources/views/wp-api/excerpt.blade.php
@@ -1,4 +1,4 @@
-<span class="tct-manager-post-excerpt"><strong>Размер: </strong>{{ $productGroup->size }} мм <br>
+<span class="tct-manager-post-excerpt"><strong>Размер: </strong>{{ $productGroup->size }} @if ($productGroup->set_pair) и <br>{{ $productGroup->set_pair->size }} @endif мм <br>
     @if ($productGroup->category->variations == 'colors')
 	    @foreach ($productGroup->products as $product)
 	    	<strong>{{ $product->main_variation_text }} </strong>– {{ $product->price }} руб/{!! $product->units_text !!} <br>
