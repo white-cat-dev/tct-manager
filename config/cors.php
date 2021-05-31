46953cd5ffec04a9796a692173b3f3e634a4f774
white-cat-dev <alexandra.19.98.study@gmail.com>
2021-05-31 22:44:18 +0800

    updated

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://tct.local', 'http://localhost:3000'],
+    'allowed_origins' => ['http://tct.local', 'http://localhost:3000', 'https://582907.ru'],
 
     'allowed_origins_patterns' => [],
 
